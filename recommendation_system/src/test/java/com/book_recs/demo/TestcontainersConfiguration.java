b4ddb20d55dc92870c42dfe050d9cb23155dca55
Daniel Coblentz <danielcoblentz916@gmail.com>
2025-10-11 13:57:01 -0400

    added base structure for app

---
+++ recommendation_system/src/test/java/com/book_recs/demo/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.book_recs.demo;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
